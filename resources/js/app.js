311768f26f018ef70fd8787c041670080cd9a540
Alexander Glück <github@alexanderglueck.at>
2019-12-06 17:18:56 +0100

    Add option to push an already sent url to the same device again

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,22 @@
 require('./bootstrap');
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const pushForm = document.getElementById('push-form');
+
+    if (!pushForm) {
+        return;
+    }
+
+    const urlInput = pushForm.querySelector('input#url');
+    const deviceSelect = pushForm.querySelector('select#device_id');
+
+    document.querySelectorAll('a.push-again-link').forEach((anchor) => {
+        anchor.addEventListener('click', (click) => {
+            click.preventDefault();
+
+            urlInput.value = anchor.dataset.url;
+            deviceSelect.value = anchor.dataset.device;
+            pushForm.submit();
+        })
+    })
+});
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,12 +18,12 @@
                             <p>Please add a device first. If you already added a device make sure you signed in on your
                                 phone at least once.</p>
                         @else
-                            <form action="{{ route('urls.store') }}" method="post">
+                            <form action="{{ route('urls.store') }}" method="post" id="push-form">
                                 @csrf
                                 <div class="input-group ">
                                     <input type="url" class="form-control @error('url') is-invalid @enderror" id="url"
                                            name="url" maxlength="500" placeholder="https://www.google.com"
-                                           aria-label="URL" required autofocus>
+                                           aria-label="URL" required>
 
                                     <select name="device_id" id="device_id" class="form-control" aria-label="Device">
                                         @foreach($devices as $device)
@@ -62,7 +62,16 @@
                                         {{ $url->url }}
                                     </a>
                                     <span class="d-flex">
-                                        <small class="flex-fill">{{ $url->device->name }}</small>
+
+                                        <small class="flex-fill">
+                                            {{ $url->device->name }}
+
+                                            @if($url->device->device_token)
+                                                <a class="push-again-link" href="#" data-url="{{ $url->url }}" data-device="{{ $url->device->id }}">
+                                                    Push again
+                                                </a>
+                                            @endif
+                                        </small>
                                         <small>{{ $url->created_at->diffForHumans() }}</small>
                                     </span>
                                 </li>
