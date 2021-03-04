7f5d4ec8421248090d03ffbd23d2f96bd9a12b33
Alexander Glück <github@alexanderglueck.at>
2021-03-04 19:47:03 +0100

    Update Laravel 8 / PHP 8

--- database/factories/DeviceFactory.php
+++ database/factories/DeviceFactory.php
@@ -1,12 +1,28 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
+namespace Database\Factories;
 
 use App\Device;
-use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
-$factory->define(Device::class, function (Faker $faker) {
-    return [
-        //
-    ];
-});
+class DeviceFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Device::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
--- database/factories/UrlFactory.php
+++ database/factories/UrlFactory.php
@@ -1,12 +1,28 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
+namespace Database\Factories;
 
 use App\Url;
-use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
-$factory->define(Url::class, function (Faker $faker) {
-    return [
-        //
-    ];
-});
+class UrlFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Url::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
