5fa32e13c72fb033e1e5b05bd2a96a9ed8d7acce
Alexander Glück <github@alexanderglueck.at>
2021-09-30 22:00:40 +0200

    Fix invalid return type

--- app/Http/Controllers/API/ApiDeviceController.php
+++ app/Http/Controllers/API/ApiDeviceController.php
@@ -3,12 +3,12 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\JsonResponse;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 
 class ApiDeviceController extends Controller
 {
-    public function index(Request $request): JsonResponse
+    public function index(Request $request): Collection
     {
         return $request->user()->devices;
     }
