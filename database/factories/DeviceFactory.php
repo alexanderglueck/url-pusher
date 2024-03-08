ab0c537f38005eb62c4c71121c35e94783547256
Alexander Glück <github@alexanderglueck.at>
2024-03-08 21:30:55 +0100

    Update database factories

--- database/factories/DeviceFactory.php
+++ database/factories/DeviceFactory.php
@@ -2,27 +2,22 @@
 
 namespace Database\Factories;
 
-use App\Models\Device;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Device>
+ */
 class DeviceFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = Device::class;
-
-    /**
      * Define the model's default state.
      *
-     * @return array
+     * @return array<string, mixed>
      */
-    public function definition()
+    public function definition(): array
     {
         return [
-            'name' => $this->faker->word
+            'name' => fake()->word
         ];
     }
 }
--- database/factories/UrlFactory.php
+++ database/factories/UrlFactory.php
@@ -2,27 +2,22 @@
 
 namespace Database\Factories;
 
-use App\Models\Url;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Url>
+ */
 class UrlFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = Url::class;
-
-    /**
      * Define the model's default state.
      *
-     * @return array
+     * @return array<string, mixed>
      */
-    public function definition()
+    public function definition(): array
     {
         return [
-            'url' => $this->faker->url
+            'url' => fake()->url
         ];
     }
 }
